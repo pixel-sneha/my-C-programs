4b5a2cccb6d3a216cfb205dc04ed1e45ac4d4357
Sne <sweetsonu2006in@gmail.com>
2025-08-07 23:30:32 +0530

    Add files via upload

---
+++ ex0202.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter a character:\n");
+    scanf("%c",&ch);
+    if(ch=='a' || ch=='A')
+    {
+        printf("entered character is a vowel\n");
+    }
+    if(ch=='e' || ch=='E')
+    {
+        printf("entered character is a vowel\n");
+    }
+    if(ch=='i' || ch=='I')
+    {
+        printf("entered character is a vowel\n");
+    }
+    if(ch=='o' || ch=='O')
+    {
+        printf("entered character is a vowel\n");
+    }
+    if(ch=='u' || ch=='U')
+    {
+        printf("entered character is a vowel\n");
+    }
+}
