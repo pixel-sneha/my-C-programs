896cae6539d4561b46f57cf23cc9af0abc1dbcae
Sne <sweetsonu2006in@gmail.com>
2025-08-13 19:28:40 +0530

    Add files via upload

---
+++ ex0302.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    //To subtract 2 matrices
+    int i,j,a[3][3],b[3][3],k,c[3][3];
+    printf("Enter matrix A values\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        scanf("%d",&a[i][j]);
+
+        }
+    }
+      printf("Enter matrix B values\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+        scanf("%d",&b[i][j]);
+
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+    //to print the subtracted array
+    printf("Array after subtraction will be:\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%5d",c[i][j]);
+        }
+    }
+    return 0;
+}
