8448eedbbe5b10c8f98af9ce4f25ee078b7f7a21
Sne <sweetsonu2006in@gmail.com>
2025-10-01 22:45:10 +0530

    Add files via upload

---
+++ Insertion_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void Insertion_sort(int arr[], int n)
+{
+    int i;
+    for(i=1;i<n;i++)
+    {
+        int temp = arr[i];
+        int j = i-1;
+       while(j>=0 && arr[j]>temp)
+       {
+        arr[j+1] = arr[j]; //arr i 
+        j--;
+       }
+       arr[j+1] = temp;
+    }   
+}
+void printArray(int arr[], int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("arr[%d] = %d\n",i,arr[i]);
+    }
+}
+int main()
+{
+    int arr[] = {4,7,8,2,1,3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array:\n");
+    printArray(arr,n);
+
+    Insertion_sort(arr,n);
+    printf("Sorted array:\n");
+    printArray(arr,n);    
+}
